14dac3d1089585fb842f32c8c5eac38a901721b9
Youthink <hufy3651@foxmail.com>
2018-07-09 03:55:26 +0800

    sidebar && tag

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo                 from './logo.svg';
 import db                   from './db';
 import cn                   from 'classnames';
 import './App.css';
@@ -26,6 +25,7 @@ class App extends Component {
 
     this.state = {
       content: '',
+      currentTag: null,
       list: [],
       tags: []
     }
@@ -33,7 +33,10 @@ class App extends Component {
 
   componentDidMount() {
     window.setTimeout(
-      this.fetchTodos.bind(this), 500
+      () => {
+        this.fetchTodos()
+        this.fetchTags()
+      }, 500
     );
   }
 
@@ -50,15 +53,34 @@ class App extends Component {
   }
 
   render() {
-    const {list, tags} = this.state;
+    const { list, tags, currentTag } = this.state;
     if(!list) return null;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-        </header>
-        <section className="main-page ui form container">
-          <h1 className="ui header">今天</h1>
+        <div className="ui visible vertical inverted menu sidebar">
+          <div className="item">
+            <div className="header">今天</div>
+          </div>
+          <div className="item">
+            <div className="header" onClick={() => this.changeTag()}>所有</div>
+          </div>
+          <div className="item">
+            <div className="header">标签</div>
+            <div className="menu">
+              {tags.map((o, i) => (
+                  <li className="ui item left icon" key={i}
+                    onClick={() => this.changeTag(o)}
+                  >
+                    {o.title}
+                  <i className="tag icon"></i>
+                </li>
+                ))
+              }
+            </div>
+          </div>
+        </div>
+        <section className="main-page ui form">
+          <h1 className="ui header">{currentTag ? currentTag : '所有'}</h1>
           <form onSubmit={this.submit.bind(this)}>
             <div className="ui left icon input">
               <input type="text"
@@ -70,8 +92,28 @@ class App extends Component {
             </div>
           </form>
           <div className="grouped fields">
-          {
-          list.map((o, i) => (
+            { currentTag ?
+              list.map((o, i) => {
+                if(o.tag === currentTag) {
+                  return(
+                    <li key={i} className="field hover-show">
+                      <span className="ui checked checkbox">
+                        <input type="checkbox" checked={o.status === 'COMPLATE'}
+                          onChange={() =>this.checkboxChange(o)}
+                        />
+                        <label className={cn({'first-three': i < 3, 'complate-status': o.status === 'COMPLATE'})}>
+                          {o.title}
+                        </label>
+                      </span>
+                      <span className="delete-btn" onClick={() => {this.delete(o)}}>
+                        <i className="times icon"></i>
+                      </span>
+                    </li>
+                  )
+                }
+              })
+            :
+            list.map((o, i) => (
             <li key={i} className="field hover-show">
               <span className="ui checked checkbox">
                 <input type="checkbox" checked={o.status === 'COMPLATE'} onChange={() =>this.checkboxChange(o)} />
@@ -86,17 +128,21 @@ class App extends Component {
           ))
           }
         </div>
-        {tags.map(o => (
-            <li>{o.title}</li>
-          ))
-        }
-          </section>
+      </section>
         </div>
     );
   }
 
   inputChange(e) {
     this.setState({content: e.target.value});
+  }
+
+  changeTag(o) {
+    let tag = null;
+    if(o) {
+      tag = o.title;
+    }
+    this.setState({currentTag: tag});
   }
 
   checkboxChange = (o) => {
@@ -124,11 +170,13 @@ class App extends Component {
   submit(e) {
     e.preventDefault();
     const inputValue = this.state.content;
+    const { currentTag } = this.state;
     const matchArr = inputValue.match(/#(.*)\s/);
+    let tag;
     if(matchArr) {
       const match = matchArr[0];
-      const item = match.slice(1, match.length-1);
-      const tag = {title: item};
+      const item = match.slice(1, match.length-1).trim();
+      tag = {title: item};
       MyDB.add('tags', tag, (e) => {
         this.fetchTags();
       });
@@ -136,10 +184,11 @@ class App extends Component {
     this.setState({content: ''});
 
     const todo = {
-      title: inputValue,
+      title: currentTag ? ('#' + currentTag + ' ' + inputValue) : inputValue,
       joinDateTime: Date.parse(new Date()),
       complateDateTime: Date.parse(new Date()),
-      status: 'INIT'
+      status: 'INIT',
+      tag: (tag && tag.title) || currentTag || ''
     };
 
     MyDB.add('todos', todo, (e) => {
